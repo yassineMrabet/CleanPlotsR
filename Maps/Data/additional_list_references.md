3ac3678f4b9a0aa073e83e23029ad15adfd6d5e8
yassineMrabet <yassine.mrabet@gmail.com>
2018-08-31 10:44:02 +0100

    Add files via upload

---
+++ Maps/Data/additional_list_references.md
@@ -0,0 +1,31 @@
+References list for the additional points [csv] (/yassineMrabet/CleanPlotsR/blob/Maps/Data/additional_list.csv)
+
+[1] Mosul, Irak : Abdelmanaa K. S. A., Aljalabi N. S. [iasj (https://www.iasj.net/iasj?func=fulltext&aId=58935) (2011) [Arabic]
+
+[2] Sabha, Libya : Abubaker, Fayza et al. Thesis [dspace.idpsebhau.edu.ly] (http://dspace.idpsebhau.edu.ly/bitstream/handle/1/317/%D9%81%D8%A7%D9%8A%D8%B2%D8%A9%20%D8%A7%D9%84%D9%84%D8%A7%D9%81%D9%8A%20%D8%A3%D8%A8%D9%88%D8%A8%D9%83%D8%B1.pdf?sequence=1&isAllowed=y (2007) [Arabic]
+
+[3] Algeria : Aoudia, Hassiba, et al.  J. Agric Food Chem 60.47(2012): 11675-11680.
+
+[4] Tamilnadu, India : Brintha, et al. Science Research Reporter 5.1(2015): 36-66.
+
+[5] Santiago, Chile : Chiffelle, Italo, et al. Chilean J. Agric Res  69.1(2009): 38-45.
+
+[6] Mumbai, Maharashtra, India : Dinesh Valke, https://www.flickr.com/photos/dinesh_valke/429546268 (2007) 
+
+[7] Riyadh, Saudi Arabia : El-Juhany, Loutfy Ibrahim. Turk J. of Agric and Forestry 35.2 (2011): 165-171.
+
+[8] Various locations, China : Liao, Boyong, et al. Forests 7.4 (2016): 81.
+
+[9] Ariana, Tunisia : M'rabet, Yassine, et al. Ind Crops Prod 107 (2017): 232-243.
+
+[10] Chlef, Algeria : Meziane, Malika, et al. IJANS 3 (2015): 173-180.
+
+[11] Dharmapuri, India : Muthamizh, S., Ramachandran V. S., Int J Bot Studies 3(2018): 109-119
+
+[12] Bhubaneswar, India : Nahak, Gayatri, et al. Nat Sci 8.4 (2010): 22-28.
+
+[13] Rajasthan, India : Sen, Antara, Amla, Batra. International Journal of Current Pharmaceutical Research 4.2(2012): 67-73.
+
+[14] Tiruchirappalli, India : Sukirtha, Raman, et al. Process Biochemistry 47.2 (2012): 273-279.
+
+[15] Baqubah, Irak: Al-Khafaji et al. Veterinary Science Development 6.1(2016).
---
+++ Maps/rgbif_sample.R
@@ -0,0 +1,128 @@
+
+# This script illustrates a simple use of 'rgbif' package
+# It generates a distribution map of species from
+# GBIF (Global Biodiversity Information Facility) records
+# See exemple in:
+# https://ropensci.org/blog/2013/07/29/rwbclimate-rgbif/
+# Please visit www.gbif.org for more details
+
+
+# Optional if above packages were already installed
+# if (!require("rgbif")) install.packages("rgbif")
+# if (!require("maps")) install.packages("maps")
+# if (!require("ggplot2")) install.packages("ggplot2")
+
+
+# Load required libraries
+library(rgbif)
+library(maps)
+library(ggplot2)
+
+
+# Import data from GBIF
+# I choosed two genera from the Meliaceae Juss. family 
+# Melia L. and a close one : Azadirachta A. Juss.
+# This may take a while
+sp.list <- occ_data(scientificName = c('Melia', 'Azadirachta'),  
+                     hasCoordinate = TRUE, limit = 5000)
+
+# Get only the name, key and coordiantes columns
+melia.data <- sp.list$Melia$data[,c(1:4)]
+# M. volkensii points were overlayed in the map, I decided to
+# plot them afterward cf. 'geom_point()' below
+melia.volk <- subset(melia.data, name == 'Melia volkensii')
+azad.data <- sp.list$Azadirachta$data[,c(1:4)]
+
+# Merge data in a single data frame and remove NA
+out <- as.data.frame(rbind(melia.data,azad.data))
+out <- out[! is.na(out$name),]
+
+# I introduced additional coordinates collected from
+# literature and personal observations
+# Check the joined csv. file
+p.add <- read.csv("~/Documents/additional_list.csv",header = TRUE)
+out <- as.data.frame(rbind(out,p.add[,c(1:4)])) 
+# melia.points <- subset(out, name = 'Melia azedarach' )
+
+# Examine the number of records 
+table(out$name)
+
+# General ggplot2 theme for the map
+# Source: https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/
+# with slight modifications 
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "Helvetica", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#fff2ebff", color = NA), 
+      panel.background = element_rect(fill = "#fff2ebff", color = NA), 
+      legend.position="bottom",
+      legend.text = element_text(face = "italic"),
+      legend.background = element_rect(fill = "#fff2ebff", color = NA),
+      panel.border = element_blank(),
+      ...
+    )
+}
+
+
+# Creating the world map
+world <- map_data("world")
+
+# Setting the native range (Indomalayan realm)
+native_range <- subset(world, region == "India" |
+                         region == "Bangladesh" |
+                         region == "Myanmar" |
+                         region == "Nepal" |
+                         region == "Malaysia" |
+                         region == "Laos" |
+                         region =="Indonesia" |
+                         region =="Java" | 
+                         region =="Bali" |
+                         region =="Cambodia" |  
+                         region =="Vietnam" |  
+                         region =="Sumatra" |  
+                         region =="Thailand" |
+                         region =="Java" |  
+                         region =="Vietnam" |  
+                         region =="Borneo")
+
+gg <- ggplot(world, aes(long, lat)) +
+      #World polygon
+      geom_polygon(aes(group = group), fill = "white", 
+               color = "gray40", size = .2)
+gg <- gg + geom_polygon(data=native_range, aes(long, lat, group =group), 
+                        fill = "#c4ac86ff" , 
+                        color = "#c4ac86ff") +
+      coord_quickmap()
+
+gg <- gg + geom_point(data = out,
+                      aes(x=decimalLongitude, y=decimalLatitude, colour=name, shape=name), 
+                      #alpha=0.5, 
+                      size = 1.5)
+
+gg <- gg + geom_point(data = melia.volk ,
+                      aes(x=decimalLongitude, y=decimalLatitude, colour=name, shape=name), 
+                      #alpha=0.5, 
+                      size = 1.5)
+
+# gg <- gg + geom_density2d(data = melia.points, aes(x = decimalLongitude, y = decimalLatitude), size = 0.3)
+
+gg <- gg + labs(color = "Species", shape="Species")
+gg <- gg + scale_color_brewer(palette="Set1")
+gg <- gg + theme_map()
+
+
+# Display the result 
+gg
+
+
+
